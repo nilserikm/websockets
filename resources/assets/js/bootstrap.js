3ff31f7f200adc1032a772d9f4e8d618a39b38d7
Nils Erik Martinussen <nils@kvalitetskontroll.no>
2018-09-26 14:33:18 +0200

    Configed base setup

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,5 +1,5 @@
 
-window._ = require('lodash');
+// window._ = require('lodash');
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -8,7 +8,7 @@ window._ = require('lodash');
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
+    // window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
 } catch (e) {}
@@ -43,13 +43,13 @@ if (token) {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo'
+import Echo from 'laravel-echo'
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '0b1d072af56fc2e6a561',
+    cluster: 'eu',
+    encrypted: true
+});
